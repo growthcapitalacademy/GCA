0cde895b1db41715dafe58ad44df88190abeee2a
growthcapitalacademy <growthcapitalacademy01@gmail.com>
2026-08-29 14:57:26 -0400

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -191,24 +191,4 @@ document.querySelectorAll('a[href^="#"]')
         });
 
     });
-const themeToggleBtn = document.getElementById('theme-toggle');
-const body = document.body;
 
-// Vérifier si un thème était déjà enregistré
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'light') {
-    body.classList.add('light-mode');
-    themeToggleBtn.textContent = '☀️';
-}
-
-themeToggleBtn.addEventListener('click', () => {
-    body.classList.toggle('light-mode');
-    
-    if (body.classList.contains('light-mode')) {
-        localStorage.setItem('theme', 'light');
-        themeToggleBtn.textContent = '☀️';
-    } else {
-        localStorage.setItem('theme', 'dark');
-        themeToggleBtn.textContent = '🌙';
-    }
-});
