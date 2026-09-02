19c71f42dd858cc8639a2ef6f1c2c590b36f0412
growthcapitalacademy <growthcapitalacademy01@gmail.com>
2026-09-02 16:37:09 -0400

    Update jssupabaseClient.js.js

--- jssupabaseClient.js.js
+++ jssupabaseClient.js.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // Remplacez par vos propres clés Supabase (disponibles dans votre tableau de bord Supabase > Project Settings > API)
-const SUPABASE_URL = 'VOTRE_URL_SUPABASE_ICI';
-const SUPABASE_ANON_KEY = 'VOTRE_CLE_ANON_SUPABASE_ICI';
+const SUPABASE_URL = 'https://supabase.com/dashboard/project/btgvcexchurmogttwnhx';
+const SUPABASE_ANON_KEY = 'sb_publishable_4AfV6LA-36Be7d98RgTYmw_NPpp66lZ';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
