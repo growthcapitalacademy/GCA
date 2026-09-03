d533d5764610553e4e88465c843906c6dc9694fb
growthcapitalacademy <growthcapitalacademy01@gmail.com>
2026-09-03 10:24:33 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // Remplacez par vos propres clés Supabase (disponibles dans votre tableau de bord Supabase > Project Settings > API)
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/btgvcexchurmogttwnhx';
+const SUPABASE_URL = 'https://btgvcexchurmogttwnhx.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_4AfV6LA-36Be7d98RgTYmw_NPpp66lZ';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
